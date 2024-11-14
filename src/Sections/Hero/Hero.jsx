9fa77e96e6c1abdeec3a7382d30c2f59e35c0cf6
Khalil RAHMOUNI <131468173+khalilrah@users.noreply.github.com>
2024-11-14 10:46:25 +0000

    Updated the site layout

--- src/Sections/Hero/Hero.jsx
+++ src/Sections/Hero/Hero.jsx
@@ -6,7 +6,7 @@ import linkdinLight from '../../assets/linkedin-light.svg'
 import linkdinDark from '../../assets/linkedin-dark.svg'
 import githubLight from '../../assets/github-light.svg'
 import githubDark from '../../assets/github-dark.svg'
-import CV from '../../assets/CV Khalil Rahmouni.pdf'
+import CV from '../../assets/CV Khalil.pdf'
 import {useTheme} from '../../common/ThemeContext'
 function Hero() {
   const {theme , toggleTheme} = useTheme(); 
